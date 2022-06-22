19aa6a798ab5d8548ba36bb3e5df94e7c6e7a1b6
Aleksey Yantsen <ayantsen@plesk.com>
2022-06-22 13:16:43 +0700

    BinarySum solution

---
+++ binarySum.php
@@ -0,0 +1,18 @@
+<?php
+
+function binarySum (string $a, string $b): string 
+{
+    $ai = bindec($a);
+    $bi = bindec($b);
+    return decbin($ai + $bi);
+}
+
+// Tests
+echo binarySum('10', '1'),  PHP_EOL ; // 11
+echo binarySum('1101', '101') ,  PHP_EOL ; // 10010
+echo binarySum('1', '1'),  PHP_EOL ; // 10
+echo binarySum('0', '0') ,  PHP_EOL ; // 0
+echo binarySum('010', '010'),  PHP_EOL ; // 10
+echo binarySum('111', '111') ,  PHP_EOL ; // 0
+echo binarySum('111', '100') ,  PHP_EOL ; // 0
+echo binarySum('111', '001') ,  PHP_EOL ; // 0
