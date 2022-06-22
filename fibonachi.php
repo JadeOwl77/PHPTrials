59d5ce42d7d75f0afdffe3a90185c3adc3b38d70
Aleksey Yantsen <ayantsen@plesk.com>
2022-06-22 13:32:59 +0700

    Fibonachi trial solution

---
+++ fibonachi.php
@@ -0,0 +1,17 @@
+<?php
+
+$fibonachi = [0, 1];
+function fib(int $n): int
+{
+    global $fibonachi;
+    if($n >= count($fibonachi)) {
+        $fibonachi[$n] = fib($n - 1) + fib($n - 2);
+    }
+    return $fibonachi[$n];
+}
+
+// Test
+// More then 92 - result in Fatal error due to int overflow
+for ($i = 0; $i <=92 ; $i++) {
+    echo "i = ", $i, " fib($i) = ", fib($i) , PHP_EOL;
+}
